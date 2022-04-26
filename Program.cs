76374d0c7e90b0bf92bdeed7534c471f1b89e12e
BakareHalimat <halimatbakare10@gmail.com>
2022-04-26 10:32:14 +0100

    week2lab

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace week2lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+             string myname = "Fuhad";
+         int myage = 16;
+         float eurostodollars = 12;
+         long  abc = 12;
+         char charVar = 'A';
+         bool boolvar = true;
+         double eurostodollar = 12;
+         dynamic phone = 45000;
+         Nullable<int> xyz = null;
+            //<<<<<Implicit conversion>>>>>>>>
+            float a = myage;
+            long b = myage;
+            //<<<<<Explicit conversion>>>>>>>>
+            double schoolfees = 12000;
+            int schoolfeesint = (int)schoolfees;
+            
+
+
+           
+            
+            Console.WriteLine(myname);
+            Console.WriteLine(myage);
+            Console.WriteLine(eurostodollars);
+            Console.WriteLine(abc);
+            Console.WriteLine(charVar);
+            Console.WriteLine(boolvar);
+            Console.WriteLine(eurostodollar);
+            Console.WriteLine(a);
+            Console.WriteLine(schoolfees);
+            Console.WriteLine(phone);
+            Console.WriteLine(xyz);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
